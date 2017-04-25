1e5a245af06bf9decdb6441eaefaced929d6d3d7
uma-yelpale <urmilayelpale@gmail.com>
2017-04-25 16:21:35 +0530

    my file

---
+++ variablesensitive.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>php</title>
+</head>
+<body>
+<?php 
+$color="red";
+echo "car color is".$color."<br>";
+echo "car color is".$COLOR."<br>";
+echo "car color is".$colOr."<br>";
+?>
+</body>
