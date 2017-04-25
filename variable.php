27adcaefd658c7fc99760c3dbc7ebd8a8f61bfe8
uma-yelpale <urmilayelpale@gmail.com>
2017-04-25 16:19:48 +0530

    my file

---
+++ variable.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<html>
+<head>
+	<title>php</title>
+</head>
+<body>
+<?php
+$txt="uma";
+$x=12;
+$y=39.8;
+echo "$txt";
+echo "<br>";
+echo "$x";
+echo "<br>";
+echo "$y"; 
+?>
+</body>
