7ea43ba9556ed2ffd09a2516fc8242f828a40b77
uma-yelpale <urmilayelpale@gmail.com>
2017-04-26 18:59:11 +0530

    my file

---
+++ foreachloop.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+$car = array("honda city","manza indigo","swift");
+foreach ($car as $value){
+	# code...
+	echo "$value <br>";
+	}
+?>
+
+</body>
+</html> 
