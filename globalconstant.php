e67df53227ae6da834d4aaa4f78726adca389f00
uma-yelpale <urmilayelpale@gmail.com>
2017-04-26 17:27:06 +0530

    my file

---
+++ globalconstant.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+	<html>
+	<head>
+		<title>php</title>
+	</head>
+	<body>
+		<?php
+			define("uma", "uma ganesh patil");
+			function mytest(){
+
+				echo uma;
+			}
+			mytest();
+		?>
+	</body>
