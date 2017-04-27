fb209f99ffa82af17103ddfeb0282fbe97ea5d8a
uma-yelpale <urmilayelpale@gmail.com>
2017-04-27 17:52:09 +0530

    my file

---
+++ simplefun.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+	<html>
+	<head>
+     <title>php</title>
+	</head>
+	<body>
+    <?Php
+     function mytest()
+     {
+     	# code...
+     	echo "uma";
+     }
+     mytest();
+
+    ?>
+</body>
+</html>
