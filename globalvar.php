a7a95644eb425cd93af686aed5fd4831399a80c3
uma-yelpale <urmilayelpale@gmail.com>
2017-04-27 18:43:20 +0530

    my file

---
+++ globalvar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+     <title>php</title>
+	</head>
+	<body>
+    <?Php
+    $x=1;
+     function mytest()
+     {
+     	# code...
+     	echo "variable is:$x";
+     }
+     mytest();
+        echo "variable is $x";
+     
+
+    ?>
+</body>
+</html>
