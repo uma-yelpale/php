8ab4d7a1f487d3f7bbd5b723ae48702bcc41d36e
uma-yelpale <urmilayelpale@gmail.com>
2017-04-27 18:34:59 +0530

    my file

---
+++ returnvaluefun.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+     <title>php</title>
+	</head>
+	<body>
+    <?Php
+     function sum($x,$y)
+     {
+        $z=$x+$y;
+        return $z;
+     	# code...
+     	;
+     }
+     echo "5+10=".sum(5,10)."<br>";
+     echo "5+20=".sum(5,20)."<br>";
+     echo "5+30=".sum(5,30)."<br>";
+     echo "5+40=".sum(5,40)."<br>";
+    ?>
+</body>
+</html>
