a0b494bdfe4f0a9dc6ce0a825b0e23bc29f4adb1
uma-yelpale <urmilayelpale@gmail.com>
2017-04-27 18:18:37 +0530

    my file

--- dowhile.php
+++ dowhile.php
@@ -5,12 +5,19 @@
 <h1>My first PHP page</h1>
 
 <?php
-$x=1;
+$x=11;
 do {
 	# code...
 	echo "number:$x <br>";
 	$x++;
+	echo "number:$x <br>";
 } while ( $x<= 10);
+
+// while($x<= 10) {
+// 		# code...
+// 	echo "number:$x <br>";
+// 	$x++;
+// }
  
 ?>
 
