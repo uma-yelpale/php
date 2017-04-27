1e652dfe78508589b657cd95169b6001d4cab589
uma-yelpale <urmilayelpale@gmail.com>
2017-04-27 18:46:07 +0530

    my file

---
+++ localvar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+     <title>php</title>
+	</head>
+	<body>
+    <?Php
+     function mytest()
+     {
+     	# code...
+        $x=5;
+     	echo "variable is: $x <br>";
+     }
+     mytest();
+        echo "variable is:$x";
+    ?>
+</body>
+</html>
