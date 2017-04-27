7ec066626d4f19dc9b2c972b6b66ff57a061cf2c
uma-yelpale <urmilayelpale@gmail.com>
2017-04-27 17:58:51 +0530

    my file

---
+++ twoargfun.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+     <title>php</title>
+	</head>
+	<body>
+    <?Php
+     function mytest($name,$age)
+     {
+     	# code...
+     	echo "uma $name is $age year old";
+     }
+     mytest("patil","25");
+
+    ?>
+</body>
+</html>
