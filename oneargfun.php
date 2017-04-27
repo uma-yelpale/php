e8c5bb86553a4008c59b768037db50d7c329a97d
uma-yelpale <urmilayelpale@gmail.com>
2017-04-27 17:55:56 +0530

    my file

---
+++ oneargfun.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+     <title>php</title>
+	</head>
+	<body>
+    <?Php
+     function mytest($name)
+     {
+     	# code...
+     	echo "uma $name";
+     }
+     mytest("patil");
+
+    ?>
+</body>
+</html>
