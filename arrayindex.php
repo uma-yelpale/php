f6a83de08abf4f6f6d75efa1cae5fd7ca3e69b79
uma-yelpale <urmilayelpale@gmail.com>
2017-05-10 15:58:42 +0530

    my file

---
+++ arrayindex.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>php</title>
+	</head>
+	<body>
+		<?php
+		$car = array("honda city","manza indigo","swift");
+		echo "i like  " .$car[0].",".$car[1]  ."  and  ".$car[2].".";
+		?>
+	</body>
+	</html>
