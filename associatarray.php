cd2fa430697e0e13dd0497e75ed94edf4c5f8f40
uma-yelpale <urmilayelpale@gmail.com>
2017-05-10 16:26:12 +0530

    my file

---
+++ associatarray.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php</title>
+</head>
+<body>
+<?php
+$my = array("name" =>"uma" ,"age" =>"25" );
+echo "yes  ".$my[name]." is  " .$my[age]."  years old";
+?>
+</body>
+</html>
