7ccab0619438dd737f7e26bc75962e74fa5dfadb
uma-yelpale <urmilayelpale@gmail.com>
2017-05-10 16:33:57 +0530

    my file

---
+++ associatarrayloop.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php</title>
+</head>
+<body>
+<?php
+$my = array("uma" =>"25" ,"ganesh" =>"28" );
+foreach ($my as $x => $x_value) {
+	# code...
+	echo "key ".$x." value ".$x_value;
+	echo "<br>";
+}
+?>
+</body>
+</html>
